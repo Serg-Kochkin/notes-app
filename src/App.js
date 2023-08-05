80ba837f6246b095ca061d51ffbebeb8f8d741fa
Serhiy Kochkin <sega84@gmail.com>
2023-08-05 22:42:45 +0300

    Added Redux Devtools compose

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import NoteEditor from './components/NoteEditor';
 import NoteList from './components/NoteList';
 import TagsList from './components/TagsList';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import notesReducer from './redux/reducers/reducers';
 
-const store = createStore(notesReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    notesReducer,
+    composeEnhancers()
+)
 
 const App = () => {
     return (
         <Provider store={store}>
             <div className='main-section'>
                 <h1>Notes Test App</h1>
-                <NoteEditor/>
+                <NoteEditor />
                 <NoteList />
                 <TagsList />
             </div>
